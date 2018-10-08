82144169f510915169e3ccf32074d7c758966994
Dida Handian <handiandida@gmail.com>
2018-10-08 22:19:13 +0700

    Program Tunjangan

---
+++ src/pbo3/pkg10117091/latihan35/programtunjangan/PBO310117091Latihan35ProgramTunjangan.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan35.programtunjangan;
+
+import java.util.Scanner;
+
+/* @author
+* NAMA  : DIDA HANDIAN
+* KELAS : PBO3
+* NIM   : 10117091
+* Deskripsi Program : 
+*/
+public class PBO310117091Latihan35ProgramTunjangan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scn = new Scanner(System.in);
+        Karyawan kar = new Karyawan();
+
+        System.out.println("===========Program Tunjangan===========");
+        System.out.print("Berapa gaji pokok Anda perbulan ? : Rp. ");
+        kar.gajiPokok = scn.nextDouble();
+        System.out.print("Status Anda? (Menikah/Belum) :");
+        kar.status = scn.next();
+
+        kar.HasilHitung(kar.status,kar.gajiPokok);
+
+    
+    }
+    
+}
